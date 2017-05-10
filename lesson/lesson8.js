932403576b07aae2cbeda5db179e1d5a2aa663fb
tadevosyan23 <raz.tadevosyan.99@mail.ru>
2017-05-10 16:37:20 +0400

    Add files via upload

---
+++ lesson/lesson8.js
@@ -0,0 +1,36 @@
+// var months = ['March' , 'April' , 'May'];
+
+// console.log(months.length);
+// var newlength = months.push('September');
+// console.log(newlength);
+// console.log(months);
+
+// var last = months.pop();
+// console.log(months);
+
+// var first = months.shift()
+// console.log(months);
+// var newlength2 = months.unshift('March')
+// console.log(months);
+
+// var position = months.indexOf('May');
+// console.log(position);
+
+//---------------
+var fruits = ["apple", "orange" , "raspberry bi"];
+var stationary = ["pen", "book" , "ruler"];
+var array = [0, 1, "span", false];
+
+// console.log(fruits,stationary,array);
+// var array1 = [fruits,stationary,array];
+// console.log(array1);
+//-----------
+
+
+var names = ["Suzy" , "Billy" , "Billy" , "Ben"];
+for (i = 0; i<names.length; i++){
+	console.log("i know someone called " + names[i]);
+}
+
+
+
