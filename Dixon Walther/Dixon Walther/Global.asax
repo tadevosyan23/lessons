5d5ebfb0205d4855fb033f0aab2b5fff882f0456
tadevosyan <you@example.com>
2017-10-05 07:58:39 +0400

    web page

---
+++ Dixon Walther/Dixon Walther/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Dixon_Walther.MvcApplication" Language="C#" %>
---
+++ Dixon Walther/Dixon Walther/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dixon_Walther.Startup))]
+namespace Dixon_Walther
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
